9fbe2817121e0355d987be71277c4639a43c02f6
alexander <inv2004@gmail.com>
2022-01-14 03:37:32 +0300

    init

---
+++ taxi.q
@@ -0,0 +1,15 @@
+\mkdir -p data
+\l data
+
+d:2017.01.01+key 365
+f:{[x;y] ([]dt:`p#x#y;ct:"x"$x?4;pc:"x"$x?9;am:"e"$x?50e;td:"e"$x?100e)}
+{(hsym `$string[.Q.par[`:.;x;`trips]],"/") set delete dt from f[380000;x]} each d
+
+\l .
+
+r:10
+min {system"t:1 select count pc by ct from trips"} each key r
+min {system"t:1 select avg am by pc from trips"} each key r
+min {system"t:1 select count ct by date.month,pc from trips"} each key r
+min {system"t:1 select count ct by date.month,pc,floor td from trips"} each key r
+
---
+++ taxi.sql
@@ -0,0 +1,19 @@
+CREATE TABLE trips (
+  DT Date,
+  CT UInt8,
+  PC UInt8,
+  AM Float32,
+  TD Float32
+) Engine = MergeTree() PARTITION BY DT ORDER BY DT SETTINGS index_granularity=8192
+AS SELECT toDate('2017-01-01')+(number div 380000) as DT, rand()%4 AS CT, rand()%9 AS PC, rand()/50%50 AS AM, rand()/100.0%100 AS TD
+     FROM numbers(365*380000);
+
+-- clickhouse-benchmark --query "SELECT COUNT() FROM trips GROUP BY CT"
+-- clickhouse-benchmark --query "SELECT PC, avg(AM) FROM trips GROUP BY PC"
+-- clickhouse-benchmark --query "SELECT PC, toMonth(DT) AS mon, count() FROM trips GROUP BY PC, mon"
+-- clickhouse-benchmark --query "SELECT PC, toMonth(DT) AS mon, round(TD) AS distance, count() FROM trips GROUP BY PC, mon, distance"
+
+-- 0.092 sec
+-- 0.238 sec
+-- 0.217 sec
+-- 0.752 sec
