2f4f1f75aa58f869b1daec4dde3438580a2a9cc8
alexander <inv2004@gmail.com>
2022-02-01 00:32:42 +0300

    unroll

--- taxi_rs/src/lib.rs
+++ taxi_rs/src/lib.rs
@@ -1,5 +1,6 @@
 use core::arch::x86_64::{__m256i, _mm256_set1_epi8, _mm256_loadu_si256, _mm256_cmpgt_epi8, _mm256_cmpeq_epi8, _mm256_movemask_epi8, _popcnt32, _mm256_extract_epi8};
 use  std::ptr::addr_of;
+use seq_macro::seq;
 
 const AVX2WIDTH: usize = 32;
 
@@ -35,38 +36,9 @@ pub unsafe fn count_group_by_avx2_unsafe(a: &[u8]) -> [i64; 256] {
     while i < a.len() - AVX2WIDTH {
         let ymm = _mm256_loadu_si256(addr_of!(a[i]) as *const __m256i);
         if 0 < _popcnt32(_mm256_movemask_epi8(_mm256_cmpgt_epi8(ymm, mask3))) {
-            result[_mm256_extract_epi8(ymm, 0) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 1) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 2) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 3) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 4) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 5) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 6) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 7) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 8) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 9) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 10) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 11) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 12) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 13) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 14) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 15) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 16) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 17) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 18) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 19) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 20) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 21) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 22) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 23) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 24) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 25) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 26) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 27) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 28) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 29) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 30) as usize] += 1;
-            result[_mm256_extract_epi8(ymm, 31) as usize] += 1;
+            seq!(N in 0..32 {
+                result[_mm256_extract_epi8(ymm, N) as usize] += 1;
+            });
         } else {
             result[0] += _popcnt32(_mm256_movemask_epi8(_mm256_cmpeq_epi8(ymm, mask0))) as i64;
             result[1] += _popcnt32(_mm256_movemask_epi8(_mm256_cmpeq_epi8(ymm, mask1))) as i64;
